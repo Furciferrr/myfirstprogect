0a813537bf14aa5781e9b8d2aa2186b2a7bf7065
Vadim <3643766@mail.ru>
2020-10-17 11:28:27 +0300

    Film Commit first

--- Script.js
+++ Script.js
@@ -1,5 +1,20 @@
 'use strict';
-let incr = 10,
-    decr = 10;
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','0');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+    },
+    genres:[],
+    privat: false,
+    };
+    let lastFilm = prompt('Один из последних просмотренных фильмов?');
+    let rating = prompt('На сколько оцените его?');
+    let lastFilm2 = prompt('Второй из последних просмотренных фильмов?');
+    let rating2Film = prompt('На сколько оцените этот фильм?');
+    personalMovieDB.movies[lastFilm] = rating;
+    personalMovieDB.movies[lastFilm2] = rating2Film;
+    console.log(personalMovieDB.movies);
     
-    console.log(incr++)
+
+
+     
